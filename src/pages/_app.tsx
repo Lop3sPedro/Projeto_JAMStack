f37764cc597ac3b054f113b5ae40b71dcd28ee45
Pedro Henrique Lopes <phs@lionx.com.br>
2022-06-08 17:53:24 -0300

    feat: create new component to study graphql

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,20 @@
 import { Provider } from "urql";
+import { Footer } from "../components/Footer";
+import { Header } from "../components/Header";
 import { client, ssrCache } from "../lib/urql";
 
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   if (pageProps.urqlState) {
-    ssrCache.restoreData(pageProps.urqlState);
+    ssrCache.restoreData(pageProps.urqlState); //faz uma verificação -> se eu já fiz uma query pelo lado do server side da aplicação e eu estiver usando os mesmos dados no client side ele não faz de novo.
   }
 
   return (
     <Provider value={client}>
+      <Header />
       <Component {...pageProps} />
+      <Footer />
     </Provider>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,22 @@
+import { GetServerSideProps } from "next";
+import { PageDocument, usePageQuery } from "../generated/graphql";
+import { client, ssrCache } from "../lib/urql";
+
 export default function Home() {
-  return <h1>Hello World</h1>;
+  const [{ data }] = usePageQuery({
+    variables: {
+      slug: "home",
+    },
+  });
+
+  return <h1>{data?.page.title}</h1>;
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  await client.query(PageDocument, { slug: "home" }).toPromise();
+  return {
+    props: {
+      urqlState: ssrCache.extractData(),
+    },
+  };
+};
